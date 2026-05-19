cc3b5b08fab4fc82fc7c89d0a746e95d02806475
forsidenis <forsidenis@yandex.ru>
2026-05-19 20:45:00 +0300

    5-config-server (сделан)

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/AnalyzerApplication.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/AnalyzerApplication.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.analyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ConfigurableApplicationContext;
 import ru.yandex.practicum.analyzer.processor.HubEventProcessor;
 import ru.yandex.practicum.analyzer.processor.SnapshotProcessor;
@@ -10,6 +11,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class AnalyzerApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(AnalyzerApplication.class, args);
